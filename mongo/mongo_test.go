e32b1f1f8497fedd007bf70b52243ca7d7504a3e
Felipe Pontes <felipemfpontes@gmail.com>
2019-07-15 21:16:39 -0300

    Migrate to Go modules
    Add CircleCI's Cache and Test Summary

--- mongo/mongo_test.go
+++ mongo/mongo_test.go
@@ -1,16 +1,28 @@
 package mongo_test
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	"github.com/jamillosantos/macchiato"
 	"github.com/lab259/rlog"
 	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	"github.com/onsi/gomega"
 )
 
 func TestMongo(t *testing.T) {
 	rlog.SetOutput(ginkgo.GinkgoWriter)
 	gomega.RegisterFailHandler(ginkgo.Fail)
-	macchiato.RunSpecs(t, "Migration Tenant Test Suite")
+	description := "Migration Tenant Test Suite"
+	if os.Getenv("CI") == "" {
+		macchiato.RunSpecs(t, description)
+	} else {
+		reporterOutputDir := "../test-results/go-migration-tenant/mongo"
+		os.MkdirAll(reporterOutputDir, os.ModePerm)
+		junitReporter := reporters.NewJUnitReporter(path.Join(reporterOutputDir, "results.xml"))
+		macchiatoReporter := macchiato.NewReporter()
+		ginkgo.RunSpecsWithCustomReporters(t, description, []ginkgo.Reporter{macchiatoReporter, junitReporter})
+	}
 }
--- mtnt_test.go
+++ mtnt_test.go
@@ -1,16 +1,30 @@
 package mtnt_test
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	"github.com/jamillosantos/macchiato"
 	"github.com/lab259/rlog"
 	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	"github.com/onsi/gomega"
 )
 
 func TestMigrationTenant(t *testing.T) {
 	rlog.SetOutput(ginkgo.GinkgoWriter)
 	gomega.RegisterFailHandler(ginkgo.Fail)
-	macchiato.RunSpecs(t, "Migration Tenant Test Suite")
+
+	description := "Migration Tenant Test Suite"
+
+	if os.Getenv("CI") == "" {
+		macchiato.RunSpecs(t, description)
+	} else {
+		reporterOutputDir := "./test-results/go-migration-tenant"
+		os.MkdirAll(reporterOutputDir, os.ModePerm)
+		junitReporter := reporters.NewJUnitReporter(path.Join(reporterOutputDir, "results.xml"))
+		macchiatoReporter := macchiato.NewReporter()
+		ginkgo.RunSpecsWithCustomReporters(t, description, []ginkgo.Reporter{macchiatoReporter, junitReporter})
+	}
 }
